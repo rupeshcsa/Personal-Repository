09f31349d1b88a9a96259037ee3428205b10730c
rupeshcsa <102574354+rupeshcsa@users.noreply.github.com>
2023-03-13 17:26:42 +0530

    upload file lwc commit

--- force-app/main/default/lwc/uploadFileFromOS/uploadFileFromOS.js
+++ force-app/main/default/lwc/uploadFileFromOS/uploadFileFromOS.js
@@ -1,61 +1,47 @@
-import { LightningElement } from 'lwc';
-import {  api,track } from 'lwc';
+import { LightningElement,api,track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import uploadFile from '@salesforce/apex/FileUploaderClass.uploadFile'
 
 import { OmniscriptBaseMixin } from 'omnistudio/omniscriptBaseMixin'
 export default class UploadFileFromOS extends OmniscriptBaseMixin(LightningElement) {
-
-
-
-    @api recordId;
-    fileData;
-    size;
-    connectedCallback(){
-        this.recordId = this.omniJsonData.DRId_Account;
-
-    }
-    openfileUpload(event) {
-       const file = event.target.files[0];
-        console.log('hello world'+ file.size);
-        this.size = file.size;
-        var reader = new FileReader()
-        reader.onload = () => {
-            var base64 = reader.result.split(',')[1]
-            this.fileData = {
-                'filename': file.name,
-                'base64': base64,
-                'recordId': this.recordId
-            }
-            console.log(this.fileData)
-        }
-        reader.readAsDataURL(file)
-
-        //this.handleClick();
-    }
     
-    handleClick(){
-     if(this.size  < 20970 ){
-            console.log('inside if'+ this.size);
-
-        const {base64, filename, recordId} = this.fileData
-        uploadFile({ base64, filename, recordId }).then(result=>{
-            
-
-           this.dispatchEvent(new ShowToastEvent({message:'file uploaded successfully 20970', varient:"success"}));
-        })
-
-        // uploadFile(this.fileData.base64, this.fileData.filename, this.fileData.recordId);
-        // this.dispatchEvent(new ShowToastEvent({message:'file uploaded successfully 20970', varient:"success"}));
-
-    }
-    else 
-    {
-        console.log('inside else');
-        this.dispatchEvent(new ShowToastEvent({message:'file  not uploaded successfully', varient:"error"}));
- 
-    }
+@track filesData= [];
+     
+get fileDataLength(){
+    return this.filesData.length!=0;
 }
-   
+openfileUpload(event) {
+    const file = event.target.files[0]
+    var reader = new FileReader()
+    reader.onload = () => {
+        var base64 = reader.result.split(',')[1]
+
+        let fileData = {
+            'filename': file.name,
+            'base64Str': base64
+            
+        }
+        
+        console.log('this.fileData ==========='+JSON.stringify(fileData));
+        this.filesData.push({'fileName':fileData.filename,'base64String':fileData.base64Str});
+       
+        this.omniUpdateDataJson(this.filesData);
+     
+        console.log('addAllFiles  === '+this.addAllFiles);
+        
+       
+    }
+   reader.readAsDataURL(file);
+}
+
+removeReceiptImage(event)
+    {
+    console.log('inside reomove ');    
+    var index = event.currentTarget.dataset.id;
+    console.log('index is ---'+index);
+    this.filesData.splice(index, 1);
+    this.omniUpdateDataJson(this.filesData);
+    }
+
+
 
 }
